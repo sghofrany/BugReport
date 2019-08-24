f0e4e8d6f4fb86b9d6b4e28cb20c40694d2478aa
Irantwomiles <irantwomiles@gmail.com>
2019-08-23 22:02:06 -0400

    typo fix

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.get('/api/bugs', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production") {
-    app.use(epxress.static('client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
