0a97e3a705ceb03c290149aef3e89bdfb288e67e
Irantwomiles <irantwomiles@gmail.com>
2019-08-23 21:20:23 -0400

    port test

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/api/bugs', (req, res) => {
     ]}  );
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}...`);
