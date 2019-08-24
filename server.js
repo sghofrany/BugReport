4245c2d5b56930aa2346651da16190b7f379f364
Irantwomiles <irantwomiles@gmail.com>
2019-08-23 21:59:25 -0400

    heroku test

--- server.js
+++ server.js
@@ -157,7 +157,15 @@ app.get('/api/bugs', (req, res) => {
     ]}  );
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === "production") {
+    app.use(epxress.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}...`);
