e3d743e107eb88d20dfab36dbe1e05130ca24708
Irantwomiles <irantwomiles@gmail.com>
2019-08-23 21:16:40 -0400

    added cocurrently

---
+++ client/build/precache-manifest.612951323023f03acbbdb91bcb79b837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2166cb469d0cb677ddda5ed315ee9e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2fc20048f2b93c71be",
+    "url": "/static/css/main.3c8045e7.chunk.css"
+  },
+  {
+    "revision": "1a6efc7ead57a70fb999",
+    "url": "/static/js/2.c3ae3d38.chunk.js"
+  },
+  {
+    "revision": "3d2fc20048f2b93c71be",
+    "url": "/static/js/main.979b437d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4b11ce8399a08e4f571c7cdef7817a9",
+    "url": "/static/media/user.c4b11ce8.jpg"
+  }
+]);
